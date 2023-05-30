8dea4d52175da6f589f5ed14326216a669e0b6c2
mohamadmelhem <mhamadmlhm0@gmail.com>
2023-05-30 11:50:38 +0300

    add th node version

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use("/uploads",express.static("./uploads"))
 if (process.env.NODE_ENV === "development"){
    app.use(morgan('dev'));
 }
